d97ae8b0a222fc93827301e8cd91f3d60a0b6c62
Lumy Kelvin <30566564+KLumy@users.noreply.github.com>
2019-11-21 21:22:22 +0900

    Revert "controller complete"
    This reverts commit 13e4e2541fc618e6fb158a4c1f3d7d6c9b65bfb2.

--- Controller/AppFactory.py
+++ Controller/AppFactory.py
@@ -1,14 +1,13 @@
 import Application
 
-
 class AppFactory():
 
     _app_module = __import__('Application')
-    _app_list = {
-        'news': 'NewsApplication',
-        'weather': 'WeatherApplication',
-        'stock': 'StockApplication',
-        'bible': 'BibleApplication',
+    _app_list = { 
+        'news' : 'NewsApplication', 
+        'weather' : 'WeatherApplication', 
+        'stock' : 'StockApplication', 
+        'bible': 'BibleApplication', 
     }
 
     def initApp(self, app_type):
@@ -17,9 +16,15 @@ class AppFactory():
         app_instance = app_class()
         return app_instance
 
-    def _append_app_list(self, app_type, app_name):
-        if app_type in AppFactory._app_list.keys():
+    def _append_app_list(self, app_type, app_name) :
+        if app_type in AppFactory._app_list.keys() :
             print('app_type {} is already in use'.format(app_type))
             raise Exception
-        else:
+        else :
             AppFactory._app_list[app_type] = app_name
+
+
+    
+
+        
+
