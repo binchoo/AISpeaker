0ef0b9c4e1c174a072a3bc570d89b0d0bd54b150
xentai <079111w@gmail.com>
2019-11-22 00:52:32 +0900

    static methodified

--- Controller/AppFactory.py
+++ Controller/AppFactory.py
@@ -11,15 +11,16 @@ class AppFactory():
 
     def initApp(self, app_type):
 
-        app_class_name = AppFactory._app_list[app_type]
+        app_class_name = self._app_list[app_type]
         app_class = getattr(Apps, app_class_name)
         app_instance = app_class()
         return app_instance
 
-    def _append_app_list(self, app_type, app_name) :
-        
+    @staticmethod
+    def append_app_list(app_type, app_name) :
+
         if app_type in AppFactory._app_list.keys() :
-            print('app_type {} is already in use'.format(app_type))
+            print('app_type {} is already in use.'.format(app_type))
             raise Exception
         else :
             AppFactory._app_list[app_type] = app_name
