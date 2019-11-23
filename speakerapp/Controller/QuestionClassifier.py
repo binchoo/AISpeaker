7d135a7a96b909802b53eacb0c299c6c3aa9f955
Lumy Kelvin <30566564+KLumy@users.noreply.github.com>
2019-11-23 15:43:57 +0900

    Revert "cummitcheck"
    This reverts commit 490a7577e8d0bbdd106f562f16febd7153e2ed9f.

--- speakerapp/Controller/QuestionClassifier.py
+++ speakerapp/Controller/QuestionClassifier.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
-
+#-*- coding: utf-8 -*-
 
 class QuestionClassifier:
     def __init__(self):
-        pass
 
     def Classify(self, ):
         pass
+
