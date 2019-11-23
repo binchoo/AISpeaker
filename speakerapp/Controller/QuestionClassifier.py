490a7577e8d0bbdd106f562f16febd7153e2ed9f
Lumy Kelvin <30566564+KLumy@users.noreply.github.com>
2019-11-23 15:42:37 +0900

    cummitcheck

--- speakerapp/Controller/QuestionClassifier.py
+++ speakerapp/Controller/QuestionClassifier.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+
 
 class QuestionClassifier:
     def __init__(self):
+        pass
 
     def Classify(self, ):
         pass
-
