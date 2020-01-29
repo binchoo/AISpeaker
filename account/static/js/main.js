58140d74e5272ebff4af8bf6c66b0b90f1430463
KLumy <lunox273@gmail.com>
2020-01-29 14:55:51 +0900

    fix error

--- account/static/js/main.js
+++ account/static/js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var model;
 var view;
 var controller;
@@ -52,51 +51,3 @@ function randomAmplitude() {
   requestAnimationFrame(randomAmplitude);
 }
 randomAmplitude();
-=======
-var model;
-var view;
-var controller;
-var stanby;
-var overlay;
-var sttManager;
-var ttsManager;
-var apiGateway;
-
-$(document).ready(function() {
-  model = new Model();
-  view = new View();
-  controller = new Controller();
-  stanby = new StandbyView();
-  overlay = new OverlayView();
-  sttManager = new STTManger();
-  ttsManager = new TTSModel();
-  apiGateway = new APIGateway("localhost");
-
-  model.objectLinker(view, controller);
-  controller.objectLinker(model, sttManager, ttsManager, apiGateway);
-  view.objectLinker(controller, [stanby, overlay]);
-  apiGateway.objectLinker(model);
-  sttManager.objectLinker(model);
-  ttsManager.objectLinker(controller);
-
-  stanby.objectLinker(view);
-  overlay.objectLinker(view);
-
-  controller.ReadFirstTimeOnLoad();
-});
-
-const siriWave = new SiriWave({
-  container: document.getElementById("siri-wave"),
-  width: 800,
-  height: 120,
-  style: "ios9",
-  autostart: true,
-  amplitude: 0
-});
-function randomAmplitude() {
-  const value = 2;
-  siriWave.setAmplitude(value);
-  requestAnimationFrame(randomAmplitude);
-}
-randomAmplitude();
->>>>>>> a89ae6cc4c453ec866e4c9b426dd967826cce4cc
